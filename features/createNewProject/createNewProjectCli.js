5eacb9ad159c40a035b3179528971c8ec31f00e2
ghromet5 <rbt@MacBook-Pro-RBT.local>
2019-09-27 19:47:03 +0700

    Add addUser function

--- features/createNewProject/createNewProjectCli.js
+++ features/createNewProject/createNewProjectCli.js
@@ -1,12 +1,27 @@
 module.exports = async () => {
   const {types} = require('../../config');
   const inquirer = require("inquirer");
+  const {addUserToProjectCli} = require('../addUserToProject');
   const {createNewProject} = require('./');
 
   const createCustomProject = async (name) => {
     const {PROJECT_URL} = await inquirer.prompt({type: "input", name: "PROJECT_URL", message: "Введите import_url"});
 
     return await createNewProject(name, PROJECT_URL)
+  }
+
+  const addUser = async (projectId) => {
+    const {ADD_USER} = await inquirer.prompt({
+      type: "list",
+      name: "ADD_USER",
+      message: "Хотите добавить в проект юзера?",
+      choices: ["Да", "Нет"]
+    });
+  
+    if (ADD_USER === "Да") {
+      await addUserToProjectCli(projectId);
+      return addUser();
+    }
   }
 
   const {PROJECT_NAME} = await inquirer.prompt({type: "input", name: "PROJECT_NAME", message: "Имя проекта"});
@@ -26,4 +41,6 @@ module.exports = async () => {
   }
 
   console.log('Ваша ссылка евгений', `https://gitlab.com/${response.path_with_namespace}`);
+
+  addUser(response.id);
 }
